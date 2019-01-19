aab49fb0821a19c023a2eb7889a99eeae0dec784
Rayce Rossum <raycer>
2019-01-19 12:57:41 -0800

    Update milestone2, failed attempts at getting heatmap working, deployment folder

--- src/app.R
+++ src/app.R
@@ -15,6 +15,7 @@ library(leaflet.extras)
 library(jsonlite)
 library(ggmap)
 library(gridExtra)
+library(rsconnect)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -63,6 +64,35 @@ server <- function(input, output, session) {
     
     colMax <- function(data) sapply(data, max, na.rm = TRUE)
     colMin <- function(data) sapply(data, min, na.rm = TRUE)
+    
+    # Data wrangling
+    data_by_year <- reactive({suicideData %>%
+            group_by(country, year) %>%
+            summarise(suicides_total = sum(suicides_no, na.rm = TRUE), pop = sum(population, na.rm = TRUE)) %>%
+            left_join(gdpData, by = c("country" = "Country or Area", "year" = "Year")) %>%
+            filter(country == input$location,
+                   year >= input$year[1],
+                   year <= input$year[2])
+    })
+    
+    data_by_sex <- reactive({suicideData %>%
+            group_by(country, year,sex) %>%
+            summarise(suicides_total = sum(suicides_no, na.rm = TRUE), pop = sum(population, na.rm = TRUE)) %>%
+            left_join(gdpData, by = c("country" = "Country or Area", "year" = "Year")) %>%
+            filter(country == input$location,
+                   year >= input$year[1],
+                   year <= input$year[2])
+    })
+    
+    data_by_age <- reactive({
+        suicideData %>%
+            filter(country == input$location,
+                   year >= input$year[1],
+                   year <= input$year[2]) %>%
+            group_by(country, age,sex) %>%
+            summarise(suicides_total = sum(suicides_no, na.rm = TRUE)) 
+    })
+    
     
     output$location = renderUI({
         selectInput("location",
@@ -138,7 +168,6 @@ server <- function(input, output, session) {
         paste(collapse = "\n") %>%
         fromJSON(simplifyVector = FALSE)
     
-    #View(geojson)
     # Default styles for all features
     geojson$style = list(
         weight = 1,
@@ -148,27 +177,40 @@ server <- function(input, output, session) {
     )
     
     # Gather GDP estimate from all countries
-    gdp_md_est <- sapply(geojson$features, function(feat) {
-        feat$properties$gdp_md_est
+    geojson_countries <- sapply(geojson$features, function(feat) {
+        feat$properties$name
     })
+    #print(geojson_countries)
+    #geojson_countries_df <- as_tibble(geojson_countries)
+    #print(suicideData)
+    #print(geojson_countries_df)
+    
+    #suicide_data_geojson <- geojson_countries %>% 
+        #join(suicideData, by = c("country" = "value"))
+    
+    #print(suicide_data_geojson)
+    
     # Gather population estimate from all countries
-    pop_est <- sapply(geojson$features, function(feat) {
-        max(1, feat$properties$pop_est)
+    pop <- sapply(geojson$features, function(feat) {
+        max(1, feat$properties$pop)
     })
     
     # Color by per-capita GDP using quantiles
-    #pal <- colorQuantile("Greens", gdp_md_est / pop_est)
+    # pal <- reactive({
+    #     colorQuantile("Greens", data_by_year()$suicides_total / data_by_year()$pop)Idk 
+    # })
     
     # Add a properties$style list to each feature
     geojson$features <- lapply(geojson$features, function(feat) {
         feat$properties$style <- list(
-            #fillColor = pal(
-            #    feat$properties$gdp_md_est / max(1, feat$properties$pop_est)
-            #)
+            # fillColor = pal(
+            #     feat$properties$suicides_total / max(1, feat$properties$pop)
+            # )
             fillColor = 1
         )
         feat
     })
+    print(geojson$features)
     
     
     output$suicideMap <- renderLeaflet({
@@ -176,40 +218,11 @@ server <- function(input, output, session) {
             addProviderTiles(providers$CartoDB.PositronNoLabels,
                              options = providerTileOptions(noWrap = TRUE)
             ) %>% 
-            addGeoJSON(geojson) %>% # TODO figure out how to make this faster
+            addGeoJSON(geojson) %>%
             setView(getSelectedLocation()$lon, getSelectedLocation()$lat, zoom=3) #TODO set zoom automatically, efficiency could be improved here as well 
             
     })
 
-    
-
-    # wrangling for the plots
-    data_by_year <- reactive({suicideData %>%
-            group_by(country, year) %>%
-            summarise(suicides_total = sum(suicides_no, na.rm = TRUE), pop = sum(population, na.rm = TRUE)) %>%
-            left_join(gdpData, by = c("country" = "Country or Area", "year" = "Year")) %>%
-            filter(country == input$location,
-                              year >= input$year[1],
-                              year <= input$year[2])
-    })
-    
-    data_by_sex <- reactive({suicideData %>%
-            group_by(country, year,sex) %>%
-            summarise(suicides_total = sum(suicides_no, na.rm = TRUE), pop = sum(population, na.rm = TRUE)) %>%
-            left_join(gdpData, by = c("country" = "Country or Area", "year" = "Year")) %>%
-            filter(country == input$location,
-                   year >= input$year[1],
-                   year <= input$year[2])
-    })
-    
-    data_by_age <- reactive({suicideData %>%
-            filter(country == input$location,
-                   year >= input$year[1],
-                   year <= input$year[2]) %>%
-            group_by(country, age,sex) %>%
-            summarise(suicides_total = sum(suicides_no, na.rm = TRUE)) 
-    })
-    
     # Line chart shows Suicide Total vs. year
     pt1 <- reactive(data_by_sex() %>% 
                                           ggplot(aes(x = year, y = suicides_total)) +
